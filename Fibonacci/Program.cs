7a4bca4ffa82e3f8f63c712f053806481c979d57
RiseGhost <jomiguelsa2mt@gmail.com>
2022-06-08 01:23:14 +0100

    Create

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+List <double> fibonacciSeries = new List<double>();
+fibonacciSeries.Add(0);
+fibonacciSeries.Add(1);
+
+//Fibonacci series with recursion:
+double fibonacci(int n){
+    if(n==0){
+        return fibonacciSeries[n];
+    }
+    if(n==1){
+        return fibonacciSeries[n];
+    }
+    fibonacciSeries.Add(fibonacciSeries[n - 1] + fibonacciSeries[n - 2]);
+    return fibonacciSeries[n];
+}
+
+for(int i = 0; i < 50; i++){
+    Console.WriteLine(i + " -> " +fibonacci(i));
+}
